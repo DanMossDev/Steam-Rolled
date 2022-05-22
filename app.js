0ee8e0bd505f0ce92d1c3396372f708925025d5c
Moss <danielsmoss96@gmail.com>
2022-05-22 01:25:52 +0100

    Create skeleton of the app, some basic initialising in the JS and a fair bit of the structure and style sorted

---
+++ app.js
@@ -0,0 +1,23 @@
+const GAMES = require('./games');
+
+//DOM cache
+const title = document.getElementById('gametitle');
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+const img3 = document.getElementById('img3');
+const bio = document.getElementById('bio');
+const swipe = document.getElementById('swipe')
+const matches = document.getElementById('matches')
+const stats = document.getElementById('stats')
+
+//variables
+let currentGame
+
+function randomGame() { //generate a random next game that the user hasn't already seen
+
+}
+
+
+function loadNext() {
+    
+}
