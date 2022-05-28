fbc0fbb007070b26d5fb553deb5a549f6f7764e5
Moss <danielsmoss96@gmail.com>
2022-05-28 13:29:08 +0100

    Update object with more games

--- games.js
+++ games.js
@@ -1,7 +1,16 @@
 const GAMES = [
-   {name: "The Sims 4",
-    ID: 1222670
-}
+   {name: "Rogue Legacy 2",
+    ID: 1253920
+   },
+   {name: "Dorfromantik",
+    ID: 1455840
+   },
+   {name: "Stardew Valley",
+    ID: 413150
+   },
+   {name: "Raft",
+    ID: 648800
+   },
 ]
 
 module.exports = GAMES;
